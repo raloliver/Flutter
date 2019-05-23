50d9fbd13fd9c2186f7bc663efe209586a3b2283
Renato <renato.forjob@gmail.com>
2019-05-23 12:49:45 -0300

    Hospedagem Flutter Web

--- projeto_telegram/lib/tela_principal.dart
+++ projeto_telegram/lib/tela_principal.dart
@@ -1,9 +1,8 @@
 import 'package:flutter_web/material.dart';
 import 'package:projeto_telegram/conversas/flutterando.dart';
-import 'package:projeto_telegram/responsive_widget.dart.dart';
 import 'package:flare_dart/actor.dart';
 import 'package:projeto_telegram/screen_resolutions/lagergeScreen.dart';
-import 'package:projeto_telegram/teste.dart';
+import 'package:projeto_telegram/util/responsive_widget.dart.dart';
 
 
 
@@ -35,78 +34,5 @@ class Tela_Inicial extends State<HomePage>{
         );
         
   }
-
-
-
-  
-
-
-
-
-Widget medium_screen(BuildContext context){
-    return  Scaffold(
-           appBar: appBar_medium(context),
-            body: Container(
-              color: Colors.white,
-            ),
-        );
-
-}
-
-
-
-Widget appBar_small(BuildContext context){
-  return new  AppBar(
-           elevation: 0.0,
-           backgroundColor: Color(0xFF5682a3),
-           leading: Padding(padding: EdgeInsets.only(top: 11, bottom: 11,left: 10),
-            child: Row(children: <Widget>[
-             Image.asset("telegram-official.png",),
-              
-           ],),
-            
-           ),
-           
-            actions: <Widget>[
-              Row(
-                children: <Widget>[
-                  Padding(
-                  padding: EdgeInsets.only(right: MediaQuery.of(context).size.width * .5, left: 0),
-                  child: Text("Telegram"),
-                  ),
-                  IconButton(icon: Icon(Icons.search, color: Colors.white,), onPressed: () {},),
-                  IconButton(icon: Icon(Icons.menu, color: Colors.white,), onPressed: () {},),
-                ],
-              )
-            ],
-           );
-}
-
-
-Widget appBar_medium(BuildContext context){
-  return new  AppBar(
-           elevation: 0.0,
-           backgroundColor: Color(0xFF5682a3),
-           leading: Padding(padding: EdgeInsets.only(top: 11, bottom: 11,left: 10),
-            child: Row(children: <Widget>[
-             Image.asset("telegram-official.png",),
-           ],),
-            
-           ),
-           
-            actions: <Widget>[
-              Row(
-                children: <Widget>[
-                  Padding(
-                  padding: EdgeInsets.only(right: MediaQuery.of(context).size.width * .7, left: 0),
-                  child: Text("Telegram"),
-                  ),
-                  IconButton(icon: Icon(Icons.search, color: Colors.white,), onPressed: () {},),
-                  IconButton(icon: Icon(Icons.menu, color: Colors.white,), onPressed: () {},),
-                ],
-              )
-            ],
-           );
-}
 
 }
